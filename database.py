ecc5aacd04b391f92fb37148010c8458110abf6b
chandu1263 <chandutargaryen@gmail.com>
2021-06-21 07:11:50 +0000

    registration code added

--- database.py
+++ database.py
@@ -1,13 +1,68 @@
-# key - value
-# key is user's discord userid
-# value is a list with username, questions attempted, questions solved, questions posted, 0, 0, 0, "", "", ""
+import mysql.connector
 
-from replit import db
+from secret import mysql_host, mysql_password, mysql_port, mysql_user
 
-def clear_database():
-  keys = db.keys()
-  for key in keys:
-    print(key, db[key])
-    del db[key]
 
-clear_database()
+def add_user_to_users(userid, username):
+    result = {}
+    try:
+        lc_db = mysql.connector.connect(
+            host = mysql_host,
+            user = mysql_user,
+            password = mysql_password,
+            database = "leetcode"
+        )
+        cursor = lc_db.cursor()
+    except Exception as e:
+        print(e)
+        result["status"] = "failure"
+        result["reason"] = "Database connection error"
+        return result
+    try:
+        query = "INSERT INTO users"
+        query += " (userid, username, attempted_questions, solved_questions) VALUES ("
+        query += "'" + str(userid) + "', '" + str(username) + "', 0, 0)"
+        cursor.execute(query, (username))
+        lc_db.commit()
+        cursor.close()
+        lc_db.close()
+        result["status"] = "success"
+        return result
+    except mysql.connector.Error as e:
+        result["status"] = "failure"
+        result["reason"] = str(e.errno) + e.msg
+        return result
+    
+def get_user_from_users(userid):
+    result = {}
+    try:
+        lc_db = mysql.connector.connect(
+            host = mysql_host,
+            user = mysql_user,
+            password = mysql_password,
+            database = "leetcode"
+        )
+        cursor = lc_db.cursor()
+    except Exception as e:
+        print(e)
+        result["status"] = "failure"
+        result["reason"] = "Database connection error"
+        return result
+    try:
+        query = "SELECT userid, username, attempted_questions, solved_questions, leetcode_username FROM users"
+        query += " WHERE userid='" + str(userid) + "'"
+        cursor.execute(query)
+        userid, username, attempted_questions, solved_questions = "", "", -1, -1
+        for userid_, username_, attempted_questions_, solved_questions_, leetcode_username_ in cursor:
+            userid = str(userid_)
+            username = str(username_)
+            attempted_questions = int(attempted_questions_)
+            solved_questions = int(solved_questions_)
+        result["status"] = "success"
+        result["value"] = [userid, username, attempted_questions, solved_questions]
+        return result
+    except Exception as e:
+        print(e)
+        result["status"] = "failure"
+        result["reason"] = "Database connection error"
+        return result
--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 import os
 import discord
 
-from replit import db
 from parser import parse, get_username
+from database import *
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-  print("We have logged in as {0.user}".format(client))
+	print("We have logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
-  
-  if message.author == client.user:
-    return
-  
-  if message.content.startswith("!lc"):
-    user_id = message.author.id
-    command = parse(message.content)
-    if command[0] == "register":
-      if len(command) > 1:
-        username = command[1]
-      else: 
-        username = ""
-      keys = db.keys()
-      print(keys)
-      if str(user_id) in keys:
-        value = db[str(user_id)]
-        await message.channel.send("User already registered with username: " + value[0])
-        return
-      else:
-        if username == "":
-          username =  message.author.name
-        db[str(user_id)] = [username, 0, 0, 0, 0, 0, 0, "", "", ""]
-        await message.channel.send("User successfully registered with username: " + username)
-        return
-
+	if message.author == client.user:
+		return
+	if message.content.startswith("!lc"):
+		user_id = message.author.id
+		command = parse(message.content)
+		user = get_user_from_users(user_id)
+		if user["status"] == "failure":
+			await message.channel.send("Bot is down, please reach out to the bot owners")
+			return
+		if user["value"][0] != "": # no user
+			await message.channel.send("User already registered with username: " + str(user["value"][1]))
+			return
+		else:
+			status = add_user_to_users(user_id, message.author.name)
+			if status["status"] == "failure":
+				await message.channel.send("User registration failed!!")
+				print(status)
+				return
+			else:
+				await message.channel.send("User successfully registered!!")
+				print(status)
+				return
+				
 
 client.run(os.getenv('TOKEN'))
 
