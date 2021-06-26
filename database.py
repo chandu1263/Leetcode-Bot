99fa78bab12d093655a6c003888977fda3e8a512
chandu1263 <chandutargaryen@gmail.com>
2021-06-26 09:49:22 +0000

    added moderator functionality

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-from secret import mysql_host, mysql_password, mysql_port, mysql_user
+from secret import mysql_host, mysql_password, mysql_port, mysql_user, mysql_db
 
 
 def add_user_to_users(userid, username):
@@ -10,7 +10,7 @@ def add_user_to_users(userid, username):
             host = mysql_host,
             user = mysql_user,
             password = mysql_password,
-            database = "leetcode"
+            database = mysql_db
         )
         cursor = lc_db.cursor()
     except Exception as e:
@@ -22,7 +22,7 @@ def add_user_to_users(userid, username):
         query = "INSERT INTO users"
         query += " (userid, username, attempted_questions, solved_questions) VALUES ("
         query += "'" + str(userid) + "', '" + str(username) + "', 0, 0)"
-        cursor.execute(query, (username))
+        cursor.execute(query)
         lc_db.commit()
         cursor.close()
         lc_db.close()
@@ -40,7 +40,7 @@ def get_user_from_users(userid):
             host = mysql_host,
             user = mysql_user,
             password = mysql_password,
-            database = "leetcode"
+            database = mysql_db
         )
         cursor = lc_db.cursor()
     except Exception as e:
@@ -60,6 +60,66 @@ def get_user_from_users(userid):
             solved_questions = int(solved_questions_)
         result["status"] = "success"
         result["value"] = [userid, username, attempted_questions, solved_questions]
+        return result
+    except Exception as e:
+        print(e)
+        result["status"] = "failure"
+        result["reason"] = "Database connection error"
+        return result
+
+
+def add_moderator(userid):
+    result = {}
+    try:
+        lc_db = mysql.connector.connect(
+            host = mysql_host,
+            user = mysql_user,
+            password = mysql_password,
+            database = mysql_db
+        )
+        cursor = lc_db.cursor()
+    except Exception as e:
+        print(e)
+        result["status"] = "failure"
+        result["reason"] = "Database connection error"
+        return result
+    try:
+        query = "INSERT INTO moderators (userid) VALUES (" + str(userid) + ")"
+        cursor.execute(query)
+        lc_db.commit()
+        cursor.close()
+        lc_db.close()
+        result["status"] = "success"
+        return result
+    except Exception as e:
+        print(e)
+        result["status"] = "failure"
+        result["reason"] = "Mentioned user is already a moderator"
+        return result     
+
+def get_moderators():
+    result = {}
+    try:
+        lc_db = mysql.connector.connect(
+            host = mysql_host,
+            user = mysql_user,
+            password = mysql_password,
+            database = mysql_db
+        )
+        cursor = lc_db.cursor()
+    except Exception as e:
+        print(e)
+        result["status"] = "failure"
+        result["reason"] = "Database connection error"
+        return result
+    try:
+        query = "SELECT * FROM moderators"
+        cursor.execute(query)
+        moderators = []
+        for id, userid in cursor:
+            moderators.append(userid)
+        result["status"] = "success"
+        result["moderators"] = moderators
         return result
     except Exception as e:
         print(e)
--- main.py
+++ main.py
@@ -12,27 +12,72 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
 	if message.author == client.user:
 		return
+	
 	if message.content.startswith("!lc"):
 		user_id = message.author.id
 		command = parse(message.content)
-		user = get_user_from_users(user_id)
-		if user["status"] == "failure":
-			await message.channel.send("Bot is down, please reach out to the bot owners")
-			return
-		if user["value"][0] != "": # no user
-			await message.channel.send("User already registered with username: " + str(user["value"][1]))
-			return
-		else:
-			status = add_user_to_users(user_id, message.author.name)
-			if status["status"] == "failure":
-				await message.channel.send("User registration failed!!")
-				print(status)
+		
+		if command[0] == "register": #reigster request
+			if len(command) > 2: #invalid request
+				await message.channel.send("Invalid request!")
+				return
+			user = get_user_from_users(user_id)
+			if user["status"] == "failure":
+				await message.channel.send("Bot is down, please reach out to the bot owners")
+				return
+			if user["value"][0] != "": # no user
+				await message.channel.send("User already registered with username: " + str(user["value"][1]))
 				return
 			else:
-				await message.channel.send("User successfully registered!!")
-				print(status)
+				username = message.author.name
+				if len(command) > 1: # user provided a username
+					username = command[1]
+				status = add_user_to_users(user_id, username)
+				if status["status"] == "failure":
+					await message.channel.send("User registration failed!!")
+					print(status)
+					return
+				else:
+					await message.channel.send("User successfully registered!!")
+					print(status)
+					return
+
+		if command[0] == "moderator": # get or add moderator
+
+			if len(command) > 2: # invalid request
+				await message.channel.send("Invalid request!")
+				return
+
+			if len(command) == 1: # get moderators
+				result = get_moderators()
+				if result["status"] == "failure":
+					await message.channel.send(result["reason"])
+					return
+				moderators = result["moderators"]
+				if len(moderators) == 0:
+					await message.channel.send("No moderators yet!")
+					return
+				mods = ""
+				for moderator in moderators:
+					moderator_profile = await client.fetch_user(int(moderator))
+					mods += moderator_profile.name + ", "
+				mods = mods[:-2]
+				await message.channel.send(mods)
+				return
+			
+			if len(command) == 2: # add moderator
+				if not message.author.guild_permissions.administrator:
+					await message.channel.send("Only admin can add a moderator!")
+					return
+				new_moderator = message.mentions[0].id
+				result = add_moderator(new_moderator)
+				if result["status"] == "failure":
+					await message.channel.send(result["reason"])
+					return
+				await message.channel.send("moderator added successfully!")
 				return
 				
 
